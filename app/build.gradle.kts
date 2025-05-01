b3cab74c8a089fa5268906531dc4923654afc421
agung <ak.exc@proton.me>
2025-05-01 16:48:52 +0700

    add leakcanary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling.debug)
     debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.leakcanary)
 }
